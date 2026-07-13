9ac6518d18bd97dbb0b389c9175c30da58aa05cf
omarkarem <omar_karem156@outlook.com>
2026-07-13 18:15:47 +0300

    create maven project

---
+++ orange-jdbc/src/main/java/com/orange/main.java
@@ -0,0 +1,7 @@
+package com.orange;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ orange-jdbc/src/test/java/com/orange/MainTest.java
@@ -0,0 +1,12 @@
+package com.orange;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
